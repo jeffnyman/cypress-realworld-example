a4ca545c055208bbecc72ad46d05d9753e81b3c2
Jeff Nyman <jeffnyman@gmail.com>
2021-04-01 12:19:08 -0500

    Addition of xhr assertions.

--- react-redux-realworld-example-app/cypress/e2e/signup.and.login.js
+++ react-redux-realworld-example-app/cypress/e2e/signup.and.login.js
@@ -2,11 +2,25 @@ describe("Authentication Process: Sign Up and Log In", () => {
     let random_string = Math.random().toString(36).substring(2)
 
     it("allows valid sign up of a new user", () => {
+        // To perform assertions against the XHR object, you need the
+        // Cypress server. Then, with the server, we look for all
+        // POST requests that hit a user request URL that has the
+        // text "users" in it.
+        cy.server()
+        cy.route("POST", "**/users").as("new_user")
+
         cy.visit("http://localhost:4100")
         cy.get(".nav-link").contains("Sign up").click()
         cy.get("[placeholder='Username']").type(`test_${random_string}`)
         cy.get("[placeholder='Email']").type(`test_${random_string}@example.com`)
         cy.get("[placeholder='Password']").type("Password1")
         cy.get("button").contains("Sign up").click()
+
+        cy.wait("@new_user")
+        cy.get("@new_user").should((xhr) => {
+            expect(xhr.status).to.eq(200)
+            expect(xhr.request.body.user.username).to.eq(`test_${random_string}`)
+            expect(xhr.request.body.user.email).to.eq(`test_${random_string}@example.com`)
+        })
     })
 })
